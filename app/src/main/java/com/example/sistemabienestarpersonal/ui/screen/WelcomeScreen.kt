b5d77ecd3ed527dd37d5806c2259f5396b6d0d79
Ezequiel Fernandez Palacios <166450934+rEzequielF@users.noreply.github.com>
2026-04-29 09:40:04 -0600

    WelcomeScreen jijiji
    papa aca hice la primera version del welcomescreen ahi despues la mejoro

--- app/src/main/java/com/example/sistemabienestarpersonal/ui/screen/WelcomeScreen.kt
+++ app/src/main/java/com/example/sistemabienestarpersonal/ui/screen/WelcomeScreen.kt
@@ -1,2 +1,59 @@
 package com.example.sistemabienestarpersonal.ui.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+
+@Composable
+fun WelcomeScreen(navController: NavController){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Text( // Titulo
+            text = "Sistema de Bienestar Personal",
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text( // Subtitulo
+            text = "Realizar un breve test psicologico para evaluar tu bienestar",
+            fontSize = 18.sp,
+            textAlign = TextAlign.Center
+        )
+        Spacer (modifier = Modifier.height(12.dp))
+
+        Text( // Descripcion
+            text = "Este test analiza aspectos como estres, motivacion y energia para darte una pequeña evaluacion personal",
+            fontSize = 14.sp,
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Button(
+            onClick = {
+                navController.navigate("test")
+            }
+        ) {
+            Text (text = "Comenzar test")
+        }
+    }
+}
