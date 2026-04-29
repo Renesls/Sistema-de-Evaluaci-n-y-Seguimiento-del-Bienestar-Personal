55f570879a5a1445ef066a2ee339843969422e03
Renesls <166040263+Renesls@users.noreply.github.com>
2026-04-29 07:42:51 -0600

    pantallas medio terminadas

---
+++ app/src/main/java/com/example/sistemabienestarpersonal/ui/navigation/NavGraph.kt
@@ -0,0 +1,25 @@
+package com.example.sistemabienestarpersonal.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.sistemabienestarpersonal.ui.screen.ResultScreen
+import com.example.sistemabienestarpersonal.ui.screen.TestScreen
+
+@Composable
+fun NavGraph() {
+    val navController = rememberNavController()
+
+    NavHost(navController, startDestination = "welcome") {
+
+        composable("test") {
+            TestScreen(navController,)
+        }
+
+        composable("result/{data}") { backStackEntry ->
+            val data = backStackEntry.arguments?.getString("data") ?: ""
+            ResultScreen(data, navController)
+        }
+    }
+}
