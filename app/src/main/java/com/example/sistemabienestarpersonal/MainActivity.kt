002c11efdf38415e87135952c6953bc429ce1368
Renesls <166040263+Renesls@users.noreply.github.com>
2026-04-29 07:54:17 -0600

    Arreglando tonteras

--- app/src/main/java/com/example/sistemabienestarpersonal/MainActivity.kt
+++ app/src/main/java/com/example/sistemabienestarpersonal/MainActivity.kt
@@ -11,6 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.sistemabienestarpersonal.ui.navigation.NavGraph
+import com.example.sistemabienestarpersonal.ui.screen.ResultScreen
+import com.example.sistemabienestarpersonal.ui.screen.TestScreen
 import com.example.sistemabienestarpersonal.ui.theme.SistemaBienestarPersonalTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +25,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SistemaBienestarPersonalTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                NavGraph()
             }
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SistemaBienestarPersonalTheme {
-        Greeting("Android")
+
+
     }
 }
