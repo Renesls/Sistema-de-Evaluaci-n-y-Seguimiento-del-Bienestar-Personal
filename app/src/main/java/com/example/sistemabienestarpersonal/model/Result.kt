2a006f3d93ac2cbd6aaf19c12e006ff23b3ec35a
Renesls <166040263+Renesls@users.noreply.github.com>
2026-05-19 17:38:58 -0600

    Removiendo archivos inutiles

--- app/src/main/java/com/example/sistemabienestarpersonal/model/Result.kt
+++
@@ -1,6 +0,0 @@
-package com.example.sistemabienestarpersonal.model
-
-data class Result(
-    val category: String,
-    val score: Int
-)
--- app/src/main/java/com/example/sistemabienestarpersonal/model/ResultUtils.kt
+++
@@ -1,9 +0,0 @@
-package com.example.sistemabienestarpersonal.model
-
-fun calculateResults(answers: List<Answer>): Map<String, Int> {
-    val grouped = answers.groupBy { it.question.category }
-
-    return grouped.mapValues { entry ->
-        entry.value.map { it.value }.average().toInt()
-    }
-}
