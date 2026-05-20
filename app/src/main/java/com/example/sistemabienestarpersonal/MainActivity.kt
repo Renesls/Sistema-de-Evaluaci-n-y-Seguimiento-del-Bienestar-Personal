5c02c58ad8ee0e4799931ff7bb0c7ba6135be4ed
Renesls <166040263+Renesls@users.noreply.github.com>
2026-05-19 18:05:15 -0600

    Errores minimos

--- app/src/main/java/com/example/sistemabienestarpersonal/MainActivity.kt
+++ app/src/main/java/com/example/sistemabienestarpersonal/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.sistemabienestarpersonal.ui.navigation.NavGraph
 import com.example.sistemabienestarpersonal.ui.theme.SistemaBienestarPersonalTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,13 +21,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SistemaBienestarPersonalTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                NavGraph()
             }
+
         }
     }
 }
